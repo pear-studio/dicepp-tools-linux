a5047b3c7a263905f0ab4e1b2d175534b5493196
pear-studio <pear_studio@163.com>
2022-02-20 15:45:37 +0800

    修复switch_to_gitee路径问题

--- tools/env/switch_to_github.sh
+++ tools/env/switch_to_github.sh
@@ -2,7 +2,6 @@
 DPP_GITEE_PATH=https://github.com/pear-studio/nonebot-dicepp.git
 DPP_TOOL_GITEE_PATH=https://github.com/pear-studio/nonebot-dicepp.git
 
-cd ../../
 git remote set-url origin $DPP_TOOL_GITEE_PATH
 cd ./DicePP 
 git remote set-url origin $DPP_GITEE_PATH
